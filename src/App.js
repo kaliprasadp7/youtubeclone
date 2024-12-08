e430f6ebf5374b7e33041d0bec916f3d1d171a3c
Kali Prasad Panda <kaliprasadp7@gmail.com>
2024-12-08 18:12:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <>
+    <h1 className="text-red-700 text-7xl">Hello World</h1>
+    <p className="text-blue-300">dsjhsdfh</p>
+    </>
+  );
+}
+
+export default App;
