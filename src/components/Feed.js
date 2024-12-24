cd69da0eaa6f96c63090b2e175a2d53ac6e803d2
Kali Prasad Panda <kaliprasadp7@gmail.com>
2024-12-24 14:23:10 +0530

    scroll to fetch more videos added

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,22 +1,30 @@
-import React,{useContext, useEffect} from 'react'
+import React, { useContext, useEffect } from 'react'
 import DataContext from '../context/DataContext'
 import VideoCard from './VideoCard'
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 function Feed() {
   const context = useContext(DataContext);
-  const {fetchVideos, data} = context;
+  const { fetchVideos, data, fetchmorevideos, totalResults } = context;
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchVideos();
     // eslint-disable-next-line
-  },[])
+  }, [])
 
   return (
+    <InfiniteScroll
+        dataLength={data.length}
+        next={fetchmorevideos}
+        hasMore={data.length < totalResults}
+        loader={<h4>Loading...</h4>}
+      >
     <div className='p-1 md:p-2 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4'>
-      {data.map((video) => {
-        return <VideoCard key={video.id} data={video}/>
-      })}
+        {data.map((video) => {
+          return <VideoCard key={video.id} data={video} />
+        })}
     </div>
+      </InfiniteScroll>
   )
 }
 
--- src/context/DataState.js
+++ src/context/DataState.js
@@ -5,6 +5,8 @@ function DataState(props) {
   const dataInitial = [];
   const host = 'https://youtube.googleapis.com/youtube/v3';
   const [data, setData] = useState(dataInitial);
+  const [totalResults, setTotalResults] = useState(0);
+  const [pageToken, setPageToken] = useState("")
   const apikey = process.env.REACT_APP_YOUTUBE_API_KEY;
 
   // const isubscribed = async () => {
@@ -22,39 +24,47 @@ function DataState(props) {
 
   const fetchVideos = async () => {
     try {
-      const response = await fetch(`${host}/videos?part=snippet,statistics&chart=mostPopular&key=${apikey}`, {
+      const response = await fetch(`${host}/videos?part=snippet,statistics&chart=mostPopular&maxResults=10&key=${apikey}`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
         },
       });
-  
+
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
-  
+
       const json = await response.json();
       if (json && json.items) {
-        json.items.map(async (video)=> {
-          const channelresponse = await fetch(`${host}/channels?part=status,snippet&id=${video.snippet.channelId}&key=${apikey}`, {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          });
-          const channeljson = await channelresponse.json();
-          // console.log(channeljson)
-          video.icon = channeljson.items[0].snippet.thumbnails.default.url;
-          video.badge = channeljson.items[0].status.isLinked;
-          // const channeldatawithvideo = video.push(icon, badge);
-          // console.log(video)
-          // video.push(icon, badge);
-          // console.log(video)
-        })
+        const enrichedVideos = await Promise.all(
+          json.items.map(async (video) => {
+            try {
+              const channelresponse = await fetch(`${host}/channels?part=status,snippet&id=${video.snippet.channelId}&key=${apikey}`, {
+                method: 'GET',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+              });
+              const channeljson = await channelresponse.json();
+              // console.log(channeljson)
+              video.icon = channeljson.items[0].snippet.thumbnails.default.url;
+              video.badge = channeljson.items[0].status.isLinked;
+            } catch (channelError) {
+              console.error('Error fetching channel details:', channelError);
+              video.icon = null;
+              video.badge = false;
+            }
+            return video;
+          })
+        );
         // console.log(channeldatawithvideo)
         // setData(channeldatawithvideo);
-        setData(json.items)
-        // console.log(json.items)
+        setData(enrichedVideos);
+        setPageToken(json.nextPageToken);
+        setTotalResults(json.pageInfo.totalResults);
+
+        // console.log(enrichedVideos)
       } else {
         console.error('No videos found in API response:', json);
         setData([]);
@@ -63,12 +73,62 @@ function DataState(props) {
       console.error('Error fetching videos:', error);
       setData([]);
     }
-  };  
+  };
+
+
+
+  const fetchmorevideos = async () => {
+    try {
+      const response = await fetch(`${host}/videos?part=snippet,statistics&chart=mostPopular&key=${apikey}&pageToken=${pageToken}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const parsedData = await response.json();
+      if (parsedData && parsedData.items) {
+        const enrichedVideos = await Promise.all(
+          parsedData.items.map(async (video) => {
+            try {
+              const channelresponse = await fetch(`${host}/channels?part=status,snippet&id=${video.snippet.channelId}&key=${apikey}`, {
+                method: 'GET',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+              });
+              const channeljson = await channelresponse.json();
+              // console.log(channeljson)
+              video.icon = channeljson.items[0].snippet.thumbnails.default.url;
+              video.badge = channeljson.items[0].status.isLinked;
+            } catch (channelError) {
+              console.error('Error fetching channel details:', channelError);
+              video.icon = null;
+              video.badge = false;
+            }
+            return video;
+          })
+        );
+
+
+        setData(data.concat(enrichedVideos)); // Concatenate new videos to the existing data
+        setPageToken(parsedData.nextPageToken);
+        setTotalResults(parsedData.pageInfo.totalResults);
+      }
+    } catch (error) {
+      console.error('Error fetching more videos:', error);
+    }
+  };
+
 
 
   return (
     <div>
-      <DataContext.Provider value={{ fetchVideos, data }}>
+      <DataContext.Provider value={{ fetchVideos, data, fetchmorevideos, totalResults }}>
         {props.children}
       </DataContext.Provider>
     </div>
